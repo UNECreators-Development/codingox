d21e64819e20a187635e014311a7155fb82f2076
Satyendra Sagar Singh <80042156+satyendra-sagar-singh@users.noreply.github.com>
2021-04-24 22:17:33 +0530

    Add files via upload

--- libraries/session.php
+++ libraries/session.php
@@ -1,14 +1,13 @@
 <?php
+    defined('APP_PATH') OR exit('No direct script access allowed');
     class session {
-        public function __construct() {
-            session_start();
-        }
-        
         public function set_session($name, $data) {
+            session_start();
             $_SESSION[$name] = $data;
         }
         
         public function get_session($name) {
+            session_start();
             if (isset($_SESSION[$name])) {
                 return $_SESSION[$name];
             }else {
@@ -16,8 +15,31 @@
             }
         }
 
-        public function del_session($name) {
+        public function unset_session($name) {
+            session_start();
             unset($_SESSION[$name]);
+        }
+
+        public function destroy_session() {
+            session_start();
+            session_destroy();
+        }
+
+        public function set_flash($name, $data) {
+            session_start();
+            $_SESSION[$name] = $data;
+        }
+
+        public function get_flash($name) {
+            $flash = "";
+            session_start();
+            if (isset($_SESSION[$name])) {
+                $flash = $_SESSION[$name];
+            }else {
+                $flash = null;
+            }
+            unset($_SESSION[$name]);
+            return $flash;
         }
     }
 ?>
