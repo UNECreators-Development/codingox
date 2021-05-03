f23f956f8aefff03662268503819abad0b79283c
Satyendra Sagar Singh <80042156+satyendra-sagar-singh@users.noreply.github.com>
2021-05-03 19:34:03 +0530

    Add files via upload

---
+++ system/Code/template.php
@@ -0,0 +1,18 @@
+<?php
+defined('APP_PATH') OR exit('No direct script access allowed');
+
+function template($title, $content='') {
+    return "<!DOCTYPE html>
+<html lang='en'>
+<head>
+    <meta charset='UTF-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <title>$title</title>
+    <link href='".APP_URL."/".APP_PATH."web/system/css/main.css' rel='stylesheet'>
+</head>
+<body>
+    $content
+</body>
+</html>";
+}
