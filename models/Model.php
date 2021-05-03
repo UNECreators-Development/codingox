f7e1b7f054d3ee7bb4ac257fcf373f4d20d6f687
Satyendra Sagar Singh <80042156+satyendra-sagar-singh@users.noreply.github.com>
2021-05-03 19:53:36 +0530

    Add files via upload

---
+++ models/Model.php
@@ -0,0 +1,10 @@
+<?php
+defined('APP_PATH') or exit('No direct script access allowed');
+//This is model file
+class Model extends Base_Model
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
