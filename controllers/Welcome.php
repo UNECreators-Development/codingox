7a656bfa37e1907707caebd0604dc79ab04f4d4f
Satyendra Sagar Singh <80042156+satyendra-sagar-singh@users.noreply.github.com>
2021-05-27 11:43:39 +0530

    Add files via upload

--- controllers/Welcome.php
+++ controllers/Welcome.php
@@ -1,5 +1,31 @@
 <?php
+
+/**
+ * CodingOx
+ *
+ * An open source application development framework for PHP
+ *
+ * This content is released under the MIT License (MIT)
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @author		Satyendra Sagar Singh
+ * @license		https://opensource.org/licenses/MIT	MIT License
+ * @link		http://framework.upgradeads.in
+ * @since		Version 1.0.0
+ * @filesource
+ **/
+
 defined('APP_PATH') or exit('No direct script access allowed');
+
+/* Default controller file */
+
 class Welcome extends Base_Controller
 {
     public function __construct()
